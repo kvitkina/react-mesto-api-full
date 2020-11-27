35bb5deb6bcdc7dda00aab29f4909fe2c84c7621
Irina Kvitkina <irina.kvitkina@gmail.com>
2020-11-27 23:43:11 +0300

    api

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -75,7 +75,7 @@ class Api {
 const api = new Api({
   baseUrl: 'https://api.kvitkina.students.nomoreparties.space/',
   headers: {
-    authorization: `Bearer ${token}`,
+    authorization: 'e63db112-531a-4c6f-ae02-ccc048d1696b',
     'Content-Type': 'application/json',
   },
 })
