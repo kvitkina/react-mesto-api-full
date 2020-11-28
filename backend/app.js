5f92effa5d59afaf2052257ce2e3b00fe94fa496
Irina Kvitkina <irina.kvitkina@gmail.com>
2020-11-28 12:37:14 +0300

    port

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 const bodyParser = require('body-parser');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
