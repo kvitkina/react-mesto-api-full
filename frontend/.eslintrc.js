d942a29873bc1754213c47459ab1420b1445f5d2
Irina Kvitkina <irina.kvitkina@gmail.com>
2020-12-02 13:12:14 +0300

    eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,11 +1,10 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
   },
   extends: 'airbnb-base',
   parserOptions: {
-    ecmaVersion: 7,
+    ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
